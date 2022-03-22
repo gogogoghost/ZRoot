a12bc4386d1644dd7f1320d89060ffab2e15100f
gogogoghost <491511934@qq.com>
2022-03-22 15:07:30 +0800

    change:action name/permission name/broadcast appOp field

--- runner/src/main/java/site/zbyte/root/Config.java
+++ runner/src/main/java/site/zbyte/root/Config.java
@@ -1,5 +1,10 @@
 package site.zbyte.root;
 
 public class Config {
+    /**
+     * Empty when use default remote class
+     * If use zRoot-builder, it will change to your custom class
+     * Runner will construct a instance at start
+     */
     public static String RemoteClass="";
 }
